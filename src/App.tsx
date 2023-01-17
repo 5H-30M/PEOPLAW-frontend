ef58539656cfb8205348a2bb4b2a02f215688546
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-01-17 20:56:34 +0900

    [STYLE] globalstyle 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createGlobalStyle } from "styled-components";
 
 function App() {
   return (
    <Router>
+    <GlobalStyle />
    </Router>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    box-sizing: border-box;
+    }
+`;
